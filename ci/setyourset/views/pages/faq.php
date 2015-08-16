cf03deb75a11db0b18037a3c0c225b2176b916c3
Jose David Cañada Aguilera <jose.bretto@gmail.com>
2015-08-16 13:29:46 +0200

    Structural update

---
+++ ci/setyourset/views/pages/faq.php
@@ -0,0 +1,80 @@
+<body background="<?php echo base_url() . 'assets/img/blitz.jpg';?>">
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">FAQ
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Content Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Where should I place the item sets?</a>
+                            </h4>
+                        </div>
+                        <div id="collapseOne" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                Simple! Just go to your LoL installation folder (Default path is C:\Riot Games\League of Legends), then go to the config folder. If the item set is global (for every champ), place it on Global\Recommended. 
+                                If the item set is just for a single champion (or severals), place it on "ChampionName"\Recommended. If any of those foldes don't exist, create it.
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">I downloaded the source code but, how it works?</a>
+                            </h4>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                If you downloaded the source code to manually create the JSON file (or if you want to edit an already existing item set), just go to the "Recommended" folder where you want to place the item set and
+                                create a new JSON file including the downloaded code (or replace the code).
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">I'm scared... This is like client modding or something? Can I be perma-banned?!</a>
+                            </h4>
+                        </div>
+                        <div id="collapseThree" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                Of course not!! Including(?) new item sets is 100% legit, so don't be afraid of create/include/share item sets :)
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">I still have more cuestions</a>
+                            </h4>
+                        </div>
+                        <div id="collapseFour" class="panel-collapse collapse">
+                            <div class="panel-body">
+                                If you have troubles or need some more information, don't hesitate to contact us -> admin@setyourset.es
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.panel-group -->
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+
+        <hr>
